fa53be97e3a4674397206f063b80df32c0033328
BartoszLukaszek <barti120@onet.pl>
2023-04-19 15:10:47 +0200

    lab 2

---
+++ lab2/src/app/small-card/small-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.scss']
+})
+export class SmallCardComponent {
+  @Input() opis = 'deg opis'
+  cena = 200
+
+}
--- lab3/src/app/app.module.ts
+++ lab3/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SmallCardComponent } from './small-card/small-card.component';
-import { MediumCardComponent } from './medium-card/medium-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SmallCardComponent,
-    MediumCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
